e4b41a56299d7d1b9142219f53f2f1c1a6d742af
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-04-23 15:44:06 -0600

    Finish assignment

---
+++ MyFirstConsoleApplication/MyFirstConsoleApplication/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MyFirstConsoleApplication
+{
+    public class Person
+    {
+        public string name;
+        public string location;
+
+        public Person()
+        {
+        }
+    }
+}
--- MyFirstConsoleApplication/MyFirstConsoleApplication/Program.cs
+++ MyFirstConsoleApplication/MyFirstConsoleApplication/Program.cs
@@ -6,7 +6,53 @@ namespace MyFirstConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            GetUserNameAndLocation();
+
+            DateTime today = DateTime.Today;
+            ChristmasCountdown(today);
+
+            GlazerApp.RunExample();
+
+            Console.WriteLine("Press any key to close the program.");
+            Console.ReadKey();
         }
+
+        private static void GetUserNameAndLocation()
+        {
+            var person = new Person();
+
+            Console.WriteLine("What is your name?: ");
+            person.name = Console.ReadLine();
+
+            Console.WriteLine($"Hi {person.name}! Where are you from? ");
+            person.location = Console.ReadLine();
+
+            Console.WriteLine($"I have never been to {person.location}. I bet it is nice. Press any key to continue...");
+            Console.ReadKey();
+            Console.WriteLine();
+
+        }
+
+        private static void ChristmasCountdown(DateTime today)
+        {
+            Console.WriteLine($"Today's date is: {today.ToString()}");
+            
+
+            DateTime christmas = DateTime.Parse($"12/25/{today.Year} 12:00:01 AM");
+
+            TimeSpan daysUntilChristmas = today - christmas;
+
+            Console.WriteLine($"There are {Convert.ToInt32(daysUntilChristmas.TotalDays) * -1} days until Christmas!");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.WriteLine();
+
+
+
+        }
+
+
     }
 }
