7334ddd504d102f6746632bdb9b61bae6211ecac
Jaden Mounteer <mounteerjaden@gmail.com>
2022-05-07 09:12:50 -0600

    Update Desk class

--- MegaDesk-Mounteer/Desk.cs
+++ MegaDesk-Mounteer/Desk.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace MegaDesk_Mounteer
 {
+
+    enum DesktopMaterial
+    {
+        laminate,
+        oak,
+        rosewood,
+        veneer,
+        pine
+    }
+
     internal class Desk
     {
+        // Fields
+        private double width;
+        private double depth;
+        private int numberOfDrawers;
+
+        // TODO Include desktop surface material here as type enum
+        private DesktopMaterial desktopMaterial;
+
+        // Properties
+        public double Width { get { return width; } set { width = value; } }
+        public double Depth { get { return depth; } set { depth = value; } }
+        public int NumberOfDrawers { get { return numberOfDrawers; } set { numberOfDrawers = value; } }
+
+        // Constructor
+        public Desk()
+        {
+            
+        }
+
     }
 }
