54864b8e7f5b78a567236c755505569299ede35a
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-06-03 07:29:07 -0600

    Add model

---
+++ RazorPagesScriptureJournal/Models/Scripture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace RazorPagesScriptureJournal.Models
+{
+    public class Scripture
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Reference { get; set; }
+
+        [Required]
+        public string Note { get; set; }
+
+        [Display(Name ="Entry Date")]
+        [DataType(DataType.Date)]
+        public DateTime EntryDate { get; set; }
+    }
+}
