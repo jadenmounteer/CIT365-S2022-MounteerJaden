df341c1acc3dbe48be8a0af110433fce5101fa6c
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-06-04 09:15:05 -0600

    Update database

---
+++ RazorPagesScriptureJournal/Migrations/20220604151339_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPagesScriptureJournal.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Scripture",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Reference = table.Column<string>(nullable: false),
+                    Note = table.Column<string>(nullable: false),
+                    EntryDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Scripture", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Scripture");
+        }
+    }
+}
