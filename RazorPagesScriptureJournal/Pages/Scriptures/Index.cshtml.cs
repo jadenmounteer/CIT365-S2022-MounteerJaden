ef97e691610df26d2b3f8bf467a5fd9452ed68b0
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-06-04 19:42:18 -0600

    Add date sorting

--- RazorPagesScriptureJournal/Pages/Scriptures/Index.cshtml.cs
+++ RazorPagesScriptureJournal/Pages/Scriptures/Index.cshtml.cs
@@ -26,22 +26,120 @@ namespace RazorPagesScriptureJournal.Pages.Scriptures
         [BindProperty(SupportsGet = true)]
         public string NoteSearch { get; set; }
 
-        public async Task OnGetAsync()
+        // Sorting
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+        public string DateSort { get; set; }
+
+        public async Task OnGetAsync(string sortOrder)
         {
 
+
+            /*** NEW CODE ***/
+
+            // Sorting
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            Console.WriteLine(sortOrder);
+
+            IQueryable<Scripture> scripturesIQ = from m in _context.Scripture
+                                                 select m;
+
+            scripturesIQ = scripturesIQ.OrderBy(s => s.EntryDate);
+
+            switch (sortOrder)
+            {
+                case "Date":
+                    scripturesIQ = scripturesIQ.OrderBy(s => s.EntryDate);
+                    break;
+
+                case "date_desc":
+                    scripturesIQ = scripturesIQ.OrderByDescending(s => s.EntryDate);
+                    break;
+
+                default:
+                    
+                    break;
+            }
+
+            Scripture = await scripturesIQ.AsNoTracking().ToListAsync();
+
+
+
+
+            // Filtering
             var scriptures = from m in _context.Scripture
-                         select m;
+                             select m;
             if (!string.IsNullOrEmpty(ReferenceSearch))
             {
                 scriptures = scriptures.Where(s => s.Reference.Contains(ReferenceSearch));
+                Scripture = await scriptures.ToListAsync();
             }
             else if (!string.IsNullOrEmpty(NoteSearch))
             {
                 scriptures = scriptures.Where(s => s.Note.Contains(NoteSearch));
-
+                Scripture = await scriptures.ToListAsync();
             }
 
-            Scripture = await scriptures.ToListAsync();
+            
+
+
+
+
+
+
+
+
+
+            ///*** OLD CODE ****/
+
+            //Console.WriteLine("Date Sort: " + DateSort);
+            //Console.WriteLine("Reference Search: " + ReferenceSearch);
+
+
+            //if (!string.IsNullOrEmpty(DateSort))
+            //{
+            //    Console.WriteLine("Sorting");
+            //    Console.WriteLine(DateSort);
+
+            //    // We are sorting
+            //    DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+
+            //    IQueryable<Scripture> scripturesIQ = from m in _context.Scripture
+            //                                         select m;
+
+            //    scripturesIQ = scripturesIQ.OrderBy(s => s.EntryDate);
+
+            //    //switch (sortOrder)
+            //    //{
+            //    //    case "Date":
+            //    //        scripturesIQ = scripturesIQ.OrderBy(s => s.EntryDate);
+            //    //        break;
+            //    //}
+
+            //    Scripture = await scripturesIQ.AsNoTracking().ToListAsync();
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Filtering");
+            //    // We are filtering
+            //    var scriptures = from m in _context.Scripture
+            //                     select m;
+            //    if (!string.IsNullOrEmpty(ReferenceSearch))
+            //    {
+            //        scriptures = scriptures.Where(s => s.Reference.Contains(ReferenceSearch));
+            //    }
+            //    else if (!string.IsNullOrEmpty(NoteSearch))
+            //    {
+            //        scriptures = scriptures.Where(s => s.Note.Contains(NoteSearch));
+
+            //    }
+
+            //    Scripture = await scriptures.ToListAsync();
+            //}
+
         }
     }
 }
