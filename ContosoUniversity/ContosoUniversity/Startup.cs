c6d6967edd422b3e214857fad53a99a4c3d5aafb
Jaden Mounteer <mounteerjaden@gmail.com>
2022-06-25 10:55:21 -0600

    Finish updates to config

--- ContosoUniversity/ContosoUniversity/Startup.cs
+++ ContosoUniversity/ContosoUniversity/Startup.cs
@@ -27,6 +27,8 @@ namespace ContosoUniversity
         {
             services.AddDbContext<SchoolContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
             services.AddControllersWithViews();
         }
 
